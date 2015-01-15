97ffa0170a73c2820e332e2d0b7272ebd28f544f
Andrey Alfaro Alvarado <andreyalfaro@gmail.com>
2015-01-15 11:46:32 -0600

    removed console.log

--- src/shapes/text.class.js
+++ src/shapes/text.class.js
@@ -832,8 +832,6 @@
       // move top offset by an ascent
       offsets.textTop += (this._fontAscent ? ((this._fontAscent / 5) * this.lineHeight) : 0);
 
-	  console.log('fabric.IText.toSvg()', offsets);
-
       this._wrapSVGTextAndBg(markup, textAndBg, shadowSpans, offsets);
 
       return reviver ? reviver(markup.join('')) : markup.join('');
